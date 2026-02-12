d1a99bc0e79a2e09bfb6b12e26cb1d3fc525d6d2
oyvindBjerke <oyvind.bjerke@toll.no>
2026-02-12 08:17:56 +0100

    Add spring boot app

---
+++ src/test/kotlin/no/obje/sse_poc/ApplicationTests.kt
@@ -0,0 +1,13 @@
+package no.obje.sse_poc
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
